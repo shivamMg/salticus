fe77deb1a08fe7da76d6ae6a75e3ba5b6af9e7a7
Shivam Mamgain <shivammamgain47@gmail.com>
2016-04-17 19:16:44 +0530

    Add InfoField for Profiles
    - InfoField is a label and its value for a Profile
    - Specify creator for a Profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Profile(models.Model):
     """
@@ -8,9 +10,22 @@ class Profile(models.Model):
     handle = models.SlugField(unique=True)
     full_name = models.CharField(max_length=30)
     description = models.TextField()
+    creator = models.ForeignKey(User)
 
     def __str__(self):
         return self.handle
+
+
+class InfoField(models.Model):
+    """
+    Field with a label and its value
+    """
+    label = models.CharField(max_length=30)
+    value = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}: {}, {}'.format(self.label, self.value[:40], self.profile)
 
 
 class Promise(models.Model):
